fa4bc52f9c9b732e96c372010059be707fb78b46
Shardul <140959449+ShardulSant@users.noreply.github.com>
2024-12-23 16:32:26 +0530

    database

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.post("/webhook", (req, res) => {
     const mobileNumber = req.headers['MobileNumber'];
     // Prepare the data for Firebase
     const data = {
-        name: userName, // Store the extracted parameter (e.g., 'name')
+        name: mobileNumber, // Store the extracted parameter (e.g., 'name')
         timestamp: new Date().toISOString(),
     };
 
